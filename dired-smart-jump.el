c4c3f56e5f730f600341d07df1c0c5da73ccfdb3
Eric Crosson <eric.s.crosson@utexas.com>
2018-11-05 21:20:46 -0600

    Match version with git tag

--- dired-smart-jump.el
+++ dired-smart-jump.el
@@ -3,7 +3,7 @@
 ;;; Copyright (C) 2018  Free Software Foundation, Inc.
 ;;
 ;; Author: Eric Crosson <eric.s.crosson@gmail.com>
-;; Version: 0.0.1
+;; Version: 1.0.0
 ;; Keywords: dired movement
 ;; Package-Requires: ((emacs "24"))
 ;; URL: https://github.com/ericcrosson/dired-smart-jump
